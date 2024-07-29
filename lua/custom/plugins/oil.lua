2116e2ac32a752d1fc67857cb0a3de0003b9f049
emptyfield <70453649+emptyfield@users.noreply.github.com>
2024-07-29 03:33:39 +0300

    feat: add oil.nvim plugin

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
+
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+}
