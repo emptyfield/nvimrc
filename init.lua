903a5258a548dc6f2fb91d2eea70d13c52857b1d
emptyfield <70453649+emptyfield@users.noreply.github.com>
2024-09-25 02:07:52 +0300

    fix: ensure required by nvim-lint markdownlint is installed

--- init.lua
+++ init.lua
@@ -674,6 +674,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'markdownlint', -- Used by nvim-lint
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
